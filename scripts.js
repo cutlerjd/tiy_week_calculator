64beabc694c0044c98239240f43b5e91c53d4ed6
Jase Cutler <jasecutler@gmail.com>
2017-07-14 15:04:39 -0700

    Small changes to layout

--- scripts.js
+++ scripts.js
@@ -2,19 +2,19 @@ let wrapper = document.getElementsByClassName('bottomButtons')[0]
 let lcd = document.getElementById('lcd')
 let clear = document.getElementById('C')
 let toggle = false
-clear.addEventListener('click', function(e){
+clear.addEventListener('click', function(event){
   lcd.innerHTML = ''
 })
 
-wrapper.addEventListener('click', function(e){
-  if(e.target.dataset.value){
+wrapper.addEventListener('click', function(event){
+  if(event.target.dataset.value){
     if(toggle){
       lcd.innerHTML = ''
       toggle = false
     }
-    let lcd_text = document.createTextNode(e.target.dataset.value)
+    let lcd_text = document.createTextNode(event.target.dataset.value)
     lcd.appendChild(lcd_text)
-  } else if(e.target.dataset.function == "=") {
+  } else if(event.target.dataset.function == "=") {
     lcd.innerHTML = eval(lcd.innerHTML)
     toggle = true
   }
