fa4607b572132c595ae7ad7293acce470db2da4b
Jase Cutler <jasecutler@gmail.com>
2017-07-14 13:48:56 -0700

    Calculator working, needs CSS help.

--- scripts.js
+++ scripts.js
@@ -1,9 +1,23 @@
-var wrapper = document.getElementsByClassName('wrapper')[0]
+let wrapper = document.getElementsByClassName('bottomButtons')[0]
+let lcd = document.getElementById('lcd')
+let clear = document.getElementById('C')
+let toggle = false
+clear.addEventListener('click', function(e){
+  lcd.innerHTML = ''
+})
 
 wrapper.addEventListener('click', function(e){
-  var clicked = e.target.dataset.value
-
-  console.log(clicked)
+  if(e.target.dataset.value){
+    if(toggle){
+      lcd.innerHTML = ''
+      toggle = false
+    }
+    let lcd_text = document.createTextNode(e.target.dataset.value)
+    lcd.appendChild(lcd_text)
+  } else if(e.target.dataset.function == "=") {
+    lcd.innerHTML = eval(lcd.innerHTML)
+    toggle = true
+  }
 })
 
 // arrayButtons.forEach(function(item){
